1c077069c57de9164403c7f15d8682b6f046ad05
Will Clardy <wclardy@kmbs.konicaminolta.us>
2018-12-04 13:14:20 -0500

    Record guard log entries

--- 2018/04/part1.rkt
+++ 2018/04/part1.rkt
@@ -4,14 +4,48 @@
 
 (struct guard-time (id total entries))
 
-(struct log-entry (id asleep awake))
+(struct log-entry (id asleep awake) #:mutable)
 
 (define log-entry-regexp
-  #px"^\\[([\\d-]+) ([\\d:]+)\\] (\\w+) (?:#(\\d+))?")
+  #px"^\\[([\\d-]+) (\\d+):(\\d+)\\] (\\w+) (?:#(\\d+))?")
+
+(define guard-log (make-hasheq))
+
+(define (new-blank-log-entry) (log-entry #f #f #f))
+
+(define (record-entry datestring hour update-entry)
+  (define log-date
+    (let ([d (parse-date datestring "yyyy-MM-dd")])
+      (if (string=? hour "23") (+days d 1) d)))
+  (define key (->posix log-date))
+  (define entry (hash-ref! guard-log key new-blank-log-entry))
+  (update-entry entry))
 
 (define (parse-entry entry)
   (match (regexp-match log-entry-regexp entry)
-    [(list _ d t "Guard" id) (log-entry (string->number id) #f #f)]
-    [(list _ d t "wakes" _)  (log-entry #f (parse-time t "HH:mm") #f)]
-    [(list _ d t "falls" _)  (log-entry #f #f (parse-time t "HH:mm") #f)]
+    [(list _ d h _ "Guard" id)
+     (record-entry d h
+                   (λ (entry)
+                     (set-log-entry-id! entry (string->number id))))]
+    [(list _ d h m "wakes" _)
+     (record-entry d h
+                   (λ (entry)
+                     (set-log-entry-awake! entry (string->number m))))]
+    [(list _ d h m "falls" _)
+     (record-entry d h
+                   (λ (entry)
+                     (set-log-entry-asleep! entry (string->number m))))]
     [_ (error "Invalid entry format")]))
+
+(parse-entry "[1518-10-18 23:51] Guard #349 begins shift")
+
+(call-with-input-file "input.txt"
+  (λ (in)
+    (for ([entry (in-lines in)])
+      (parse-entry entry))))
+
+(for ([entry (in-hash-values guard-log)])
+  (printf "Guard: ~a, Asleep: ~a, Awake: ~a\n"
+          (log-entry-id entry)
+          (log-entry-asleep entry)
+          (log-entry-awake entry)))
