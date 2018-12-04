756e98748f280f66c5421d4c131ca6a01c04c0c7
Will Clardy <will@quexxon.net>
2018-12-04 01:57:50 -0500

    Refactor boxed sets to eliminate top-level unbox

--- 2018/03/part1.rkt
+++ 2018/03/part1.rkt
@@ -8,9 +8,9 @@
 (call-with-input-file "input.txt"
   (λ (in)
     (time
-     (define visited-points-bx (box (mutable-seteq)))
-     (define overlap-points-bx (box (mutable-seteq)))
+     (define visited-points (mutable-seteq))
+     (define overlap-points (mutable-seteq))
      (for ([line (in-lines in)])
        (define c (parse-claim line))
-       (record-claim c visited-points-bx overlap-points-bx))
-     (set-count (unbox overlap-points-bx)))))
+       (record-claim c (box visited-points) (box overlap-points)))
+     (set-count overlap-points))))
