a42541490fb896ddc4d86f3f2f838ec424f44f09
Will Clardy <will@quexxon.net>
2019-04-10 22:00:14 -0400

    Add ocamlformat support and format

--- ocaml/2018/01/main.ml
+++ ocaml/2018/01/main.ml
@@ -2,36 +2,30 @@ open Core
 open Core_bench
 open Stdio
 
-let part_one = List.fold ~f:(+) ~init:0
+let part_one = List.fold ~f:( + ) ~init:0
 
 let part_two ints =
   let size = Int.max 10_000 (List.length ints) in
   let prev_sums = Hash_set.create (module Int) ~size () in
   Sequence.cycle_list_exn ints
   |> Sequence.fold_until ~init:0
-    ~finish:(fun _ -> assert false)
-    ~f:(fun prev_sum int ->
-        let new_sum = prev_sum + int in
-        if Hash_set.mem prev_sums new_sum then
-          Stop new_sum
-        else begin
-          Hash_set.add prev_sums new_sum;
-          Continue new_sum
-        end
-      )
+       ~finish:(fun _ -> assert false)
+       ~f:(fun prev_sum int ->
+         let new_sum = prev_sum + int in
+         if Hash_set.mem prev_sums new_sum then Stop new_sum
+         else (
+           Hash_set.add prev_sums new_sum ;
+           Continue new_sum ) )
 
 let () =
   In_channel.with_file "input.txt" ~f:(fun inc ->
       let ints = List.map ~f:Int.of_string (In_channel.input_lines inc) in
       let exec_part_one () = part_one ints in
       let exec_part_two () = part_two ints in
-      print_endline "RESULTS:";
-      printf "Part 1: %d\n" (exec_part_one ());
-      printf "Part 2: %d\n" (exec_part_two ());
-      print_endline "\nBENCHMARKS:";
-      [ Bench.Test.create ~name:"Part 1" exec_part_one;
-        Bench.Test.create ~name:"Part 2" exec_part_two;
-      ]
-      |> Bench.make_command
-      |> Command.run
-    )
+      print_endline "RESULTS:" ;
+      printf "Part 1: %d\n" (exec_part_one ()) ;
+      printf "Part 2: %d\n" (exec_part_two ()) ;
+      print_endline "\nBENCHMARKS:" ;
+      [ Bench.Test.create ~name:"Part 1" exec_part_one
+      ; Bench.Test.create ~name:"Part 2" exec_part_two ]
+      |> Bench.make_command |> Command.run )
--- ocaml/2018/02/main.ml
+++ ocaml/2018/02/main.ml
@@ -2,18 +2,15 @@ open Core
 open Core_bench
 open Stdio
 
-
 (* -- HELPERS --------------------------------------------------------------- *)
 
-
 let box_value box =
-  let has_count n counts = Bool.to_int (List.exists ~f:((=) n) counts) in
+  let has_count n counts = Bool.to_int (List.exists ~f:(( = ) n) counts) in
   let counts = Hashtbl.create (module Char) ~size:(String.length box) in
   String.iter box ~f:(fun k ->
-      Hashtbl.update counts k ~f:(fun v -> 1 + Option.value ~default:0 v)
-    );
+      Hashtbl.update counts k ~f:(fun v -> 1 + Option.value ~default:0 v) ) ;
   let counts = Hashtbl.data counts in
-  ( has_count 2 counts, has_count 3 counts )
+  (has_count 2 counts, has_count 3 counts)
 
 let string_zip_exn s1 s2 =
   let n = String.length s1 in
@@ -28,34 +25,30 @@ let one_char_diff_exn s1 s2 =
     if i = n then diff_chars = 1
     else if Char.(s1.[i] <> s2.[i]) then
       if diff_chars = 1 then false else loop (i + 1) (diff_chars + 1)
-    else
-      loop (i + 1) diff_chars
+    else loop (i + 1) diff_chars
   in
   loop 0 0
 
 let keep_shared_chars s1 s2 =
   let char_lists = string_zip_exn s1 s2 in
-  List.fold_right char_lists ~init:[] ~f:(fun (x,y) acc ->
-      if Char.(x = y) then x :: acc else acc
-    )
+  List.fold_right char_lists ~init:[] ~f:(fun (x, y) acc ->
+      if Char.(x = y) then x :: acc else acc )
   |> String.of_char_list
-
 
 (* -- AOC 2018 DAY 2 -------------------------------------------------------- *)
 
-
 let part_one boxes =
-  let sum (x,y) (x',y') = (x + x', y + y') in
-  List.fold boxes ~init:(0,0) ~f:(fun ac box -> sum ac (box_value box))
-  |> (fun (twos, threes) -> twos * threes)
+  let sum (x, y) (x', y') = (x + x', y + y') in
+  List.fold boxes ~init:(0, 0) ~f:(fun ac box -> sum ac (box_value box))
+  |> fun (twos, threes) -> twos * threes
 
 let part_two boxes ~default =
   let rec loop = function
     | [] -> default
-    | box :: boxes ->
+    | box :: boxes -> (
       match List.find boxes ~f:(one_char_diff_exn box) with
       | None -> loop boxes
-      | Some box' -> keep_shared_chars box box'
+      | Some box' -> keep_shared_chars box box' )
   in
   loop boxes
 
@@ -64,13 +57,10 @@ let () =
       let lines = In_channel.input_lines inc in
       let exec_part_one () = part_one lines in
       let exec_part_two () = part_two lines ~default:"" in
-      print_endline "RESULTS:";
-      printf "Part 1: %d\n" (exec_part_one ());
-      printf "Part 2: %s\n" (exec_part_two ());
-      print_endline "\nBENCHMARKS:";
-      [ Bench.Test.create ~name:"Part 1" exec_part_one;
-        Bench.Test.create ~name:"Part 2" exec_part_two;
-      ]
-      |> Bench.make_command
-      |> Command.run
-    )
+      print_endline "RESULTS:" ;
+      printf "Part 1: %d\n" (exec_part_one ()) ;
+      printf "Part 2: %s\n" (exec_part_two ()) ;
+      print_endline "\nBENCHMARKS:" ;
+      [ Bench.Test.create ~name:"Part 1" exec_part_one
+      ; Bench.Test.create ~name:"Part 2" exec_part_two ]
+      |> Bench.make_command |> Command.run )
